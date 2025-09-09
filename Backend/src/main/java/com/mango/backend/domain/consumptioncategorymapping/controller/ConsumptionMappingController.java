9aaa33565791d5ed0ee15749ec3d87b653f48d4f
소태호 <sth9057@gmail.com>
2025-09-09 20:15:13 +0900

    feat : 실행안되는 문제 해결 - Auth와 User로 도메인 분리 - 잘못 매핑되어 있는 RedisTemplate 수정

--- Backend/src/main/java/com/mango/backend/domain/consumptioncategorymapping/controller/ConsumptionMappingController.java
+++ Backend/src/main/java/com/mango/backend/domain/consumptioncategorymapping/controller/ConsumptionMappingController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/mapping")
 @RequiredArgsConstructor
-public class UserController {
-  
+public class ConsumptionMappingController {
+
 }
--- Backend/src/main/java/com/mango/backend/domain/user/service/UserService.java
+++ Backend/src/main/java/com/mango/backend/domain/user/service/UserService.java
@@ -6,10 +6,8 @@ import com.mango.backend.global.common.api.ApiResponse;
 import com.mango.backend.global.common.api.ErrorResponse;
 import com.mango.backend.global.common.api.SuccessResponse;
 import com.mango.backend.global.error.ErrorCode;
-import com.mango.backend.global.util.JwtProvider;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
   private final UserRepository userRepository;
-  private final JwtProvider jwtProvider;
-  private final RedisTemplate<String, User> redisTemplate;
 
   @Transactional
   public ApiResponse<?> deleteUser(Long userId) {
