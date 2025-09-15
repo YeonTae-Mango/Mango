4bb83d8319f191de11ea797ba13eca4827c1b9f8
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-09-15 16:43:28 +0900

    Merge backend code from BE branch

---
+++ Backend/src/main/java/com/mango/backend/domain/mango/entity/Mango.java
@@ -0,0 +1,37 @@
+package com.mango.backend.domain.mango.entity;
+
+import com.mango.backend.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "mangos")
+public class Mango {
+
+  @Id
+  @Column(name = "like_id", nullable = false)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "from_id", nullable = false)
+  private User from;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "to_id", nullable = false)
+  private User to;
+
+}
