0f463587eb36aba17fdfbc3ca867a9a2c07b8fdc
skdbsqls <skdbsqls98@naver.com>
2025-09-18 12:22:32 +0900

    Feat: 앱 메타데이터 설정 및 스플래시 화면 구현중

--- Frontend/mango/src/navigation/AuthStack.tsx
+++ Frontend/mango/src/navigation/AuthStack.tsx
@@ -1,9 +1,8 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import LoginScreen from '../screens/login/LoginScreen';
-import SignupScreen from '../screens/signup/SignupScreen';
 import SignupCompleteScreen from '../screens/signup/SignupCompleteScreen';
-import BaseScreen from '../screens/start/BaseScreen';
+import SignupScreen from '../screens/signup/SignupScreen';
 
 const Stack = createStackNavigator();
 
@@ -15,10 +14,9 @@ export default function AuthStack({ onLoginSuccess }: AuthStackProps) {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerShown: false, // Layout 컴포넌트에서 헤더 처리
+        headerShown: false,
       }}
     >
-      <Stack.Screen name="Base" component={BaseScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="SignupComplete">
