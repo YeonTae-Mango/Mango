ab8e2a173ebe8a7c345ef33352d11eb5edeb3c5c
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-09-18 15:04:50 +0900

    feat: 엔티티, 리포지토리 MongoDB 사용하도록 변경

--- Backend/src/main/java/com/mango/backend/domain/paymenthistory/repository/PaymentHistoryRepository.java
+++ Backend/src/main/java/com/mango/backend/domain/paymenthistory/repository/PaymentHistoryRepository.java
@@ -1,13 +1,13 @@
 package com.mango.backend.domain.paymenthistory.repository;
 
 import com.mango.backend.domain.paymenthistory.entity.PaymentHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface PaymentHistoryRepository extends JpaRepository<PaymentHistory,Long> {
-    List<PaymentHistory> findByUserIdAndPaymentDateBetween(Long userId, Instant startDate, Instant endDate);
+public interface PaymentHistoryRepository extends MongoRepository<PaymentHistory,String> {
+    List<PaymentHistory> findByUserIdAndPaymentTimeBetween(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 }
