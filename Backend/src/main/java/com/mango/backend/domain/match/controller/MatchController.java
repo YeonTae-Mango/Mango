ddc5167939f1d86ee12a5a8cb2b75910a2e9eb87
소태호 <sth9057@gmail.com>
2025-09-21 17:12:35 +0900

    feat : 매치 카테고리 추가
    - 스와이프 목록을 조회할 때 전체 목록을 받아올 수 있는 기능을 유지하며 카테고리 별로 필터링해서 가져올 수 있도록 하였습니다.

--- Backend/src/main/java/com/mango/backend/domain/match/controller/MatchController.java
+++ Backend/src/main/java/com/mango/backend/domain/match/controller/MatchController.java
@@ -6,11 +6,8 @@ import com.mango.backend.global.common.api.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @RestController
 @RequestMapping("/api/v1/match")
@@ -19,17 +16,24 @@ public class MatchController extends BaseController {
 
   private final MatchService matchService;
 
+
   @Operation(
-      summary = "유저 목록 조회 (임시 목데이터 사용중)",
-      description = "주어진 위도/경도를 기준으로 가까운 순으로 유저 목록을 조회합니다. " +
-          "한 페이지당 10개씩 반환하며(아직 안됨, 일단 다 주는 중;), 검색 결과가 없으면 빈 리스트를 반환합니다."
+          summary = "유저 목록 조회 (임시 목데이터 사용중)",
+          description = "주어진 기준으로 가까운 순으로 유저 목록을 조회합니다. " +
+                  "카테고리를 선택하면 해당 유형만 필터링하며, 검색 결과가 없으면 빈 리스트를 반환합니다."
   )
-  @GetMapping("/swipe/{userId}")
+  @GetMapping("/swipe")
   public ResponseEntity<BaseResponse> getNearbyUsers(
-      @RequestHeader("Authorization") String token,
-      @PathVariable Long userId) {
-    return toResponseEntity(matchService.getSwipeList(token, userId),
-        "근처 사용자 목록 조회에 성공하였습니다.");
+          @RequestHeader("Authorization") String token,
+          @Parameter(description = "조회할 유저 ID", required = true)
+          @RequestParam Long userId,
+          @Parameter(description = "선택 카테고리 (optional)")
+          @RequestParam(required = false) String category
+  ) {
+    return toResponseEntity(
+            matchService.getSwipeList(token, userId, category),
+            "근처 사용자 목록 조회에 성공하였습니다."
+    );
   }
 
 }
