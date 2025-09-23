8b57c65881367741a2392c88df49ab769420a580
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-09-23 15:28:05 +0900

    feat: 매치 된 사람 제거하여 나를 망고한사람, 내가 망고한사람 조회

--- Backend/src/main/java/com/mango/backend/domain/mango/repository/MangoRepository.java
+++ Backend/src/main/java/com/mango/backend/domain/mango/repository/MangoRepository.java
@@ -2,12 +2,12 @@ package com.mango.backend.domain.mango.repository;
 
 import com.mango.backend.domain.mango.entity.Mango;
 import com.mango.backend.domain.user.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MangoRepository extends JpaRepository<Mango, Long> {
 
@@ -18,7 +18,7 @@ public interface MangoRepository extends JpaRepository<Mango, Long> {
           LEFT JOIN FETCH u.profilePhoto
           WHERE m.from.id = :userId
       """)
-  Page<User> findUsersILikedWithProfile(Long userId, Pageable pageable);
+  Page<User> findUsersILikedWithProfile(@Param("userId")Long userId, Pageable pageable);
 
   // 나를 망고한 사람들 (userId 기준, 페이징)
   @Query("""
@@ -27,7 +27,7 @@ public interface MangoRepository extends JpaRepository<Mango, Long> {
           LEFT JOIN FETCH u.profilePhoto
           WHERE m.to.id = :userId
       """)
-  Page<User> findUsersWhoLikedMeWithProfile(Long userId, Pageable pageable);
+  Page<User> findUsersWhoLikedMeWithProfile(@Param("userId")Long userId, Pageable pageable);
 
   @Query("select m.to.id from Mango m where m.from.id = :userId")
   Set<Long> findILikedUserIds(@Param("userId") Long userId);
